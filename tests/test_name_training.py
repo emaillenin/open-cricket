5ebd1103928cc1c1a0387d431d925d11fb8670bf
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-01-05 20:20:02 +0530

    Try with dir name

--- tests/test_name_training.py
+++ tests/test_name_training.py
@@ -7,7 +7,7 @@ from corpus.training.cricket_players import TrainCricketPlayers
 class TestNameTraining(unittest.TestCase):
 
     def test_simple_names(self):
-        training = TrainCricketPlayers(os.path.join(os.path.realpath('.'), 'data', 'player_names.pickle'))
+        training = TrainCricketPlayers(os.path.join(os.path.dirname(__file__), 'data', 'player_names.pickle'))
         tagged_words = training.get_names('what is the best score of dhoni and virat')
         player_names = [p[0] for p in tagged_words if p[1] == 'NNP']
         self.assertListEqual(player_names, ['dhoni', 'virat'])
