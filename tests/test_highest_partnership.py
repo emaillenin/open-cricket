aba1e84ed842d6f56bb3c780cd2bdff78567e11a
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-03-29 12:29:23 +0530

    #BuildFix

--- tests/test_highest_partnership.py
+++ tests/test_highest_partnership.py
@@ -7,7 +7,7 @@ class TestPartnership(unittest.TestCase):
 
     def setUp(self):
         self.input = 'highest partnerships for 1st wicket for south africa'
-        self.expected = '{"partnerships": {"wkt_order": "1st", "filler": "for", "wicket": "wicket", "extent": "highest", "team": {"team1": "south", "team2": "africa"}, "select": "partnerships"}}'
+        self.expected = '{"partnerships": {"wkt_order": "1st", "filler": "for", "wicket": "wicket", "word_extent": "highest", "team": {"team1": "south", "team2": "africa"}, "select": "partnerships"}}'
 
     def test_search(self):
         parser = SentenceParser(self.input)
