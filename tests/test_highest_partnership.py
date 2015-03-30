27fd297e1a87483c3d783f5ea987562eacd14845
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-03-30 19:31:36 +0530

    #BuildFix

--- tests/test_highest_partnership.py
+++ tests/test_highest_partnership.py
@@ -6,8 +6,8 @@ from opencricket.chart.sentence_parser import SentenceParser
 class TestPartnership(unittest.TestCase):
 
     def setUp(self):
-        self.input = 'highest partnerships for 1st wicket for south africa'
-        self.expected = '{"partnerships": {"wkt_order": "1st", "filler": "for", "wicket": "wicket", "word_extent": "highest", "team": {"team1": "south", "team2": "africa"}, "select": "partnerships"}}'
+        self.input = 'highest partnership for 1st wicket for south africa'
+        self.expected = '{"partnerships": {"word_wkt_order": "1st", "word_wicket": "wicket", "word_for": "for", "word_extent": "highest", "team": {"team1": "south", "team2": "africa"}, "word_partnership": "partnership"}}'
 
     def test_search(self):
         parser = SentenceParser(self.input)
