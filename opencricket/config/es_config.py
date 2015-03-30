64680592d98b9d485b85c40df00eade6bba486ea
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-03-30 19:23:34 +0530

    Remove html_strip from filter in ElasticSearch

--- opencricket/config/es_config.py
+++ opencricket/config/es_config.py
@@ -1,6 +1,7 @@
+import logging
 from elasticsearch import Elasticsearch
 
-index_settings='{  "settings": {    "index": {      "analysis": {        "filter": {          "stemmer": {            "type": "stemmer",            "language": "english"          },          "autocompleteFilter": {            "max_shingle_size": "5",            "min_shingle_size": "2",            "type": "shingle"          },          "stopwords": {            "type": "stop",            "stopwords": [              "_english_"            ]          }        },        "analyzer": {          "didYouMean": {            "filter": [              "lowercase"            ],            "char_filter": [              "html_strip"            ],            "type": "custom",            "tokenizer": "standard"          },          "autocomplete": {            "filter": [              "lowercase",              "autocompleteFilter"            ],            "char_filter": [              "html_strip"            ],            "type": "custom",            "tokenizer": "standard"          },          "default": {            "filter": [              "lowercase",              "stopwords",              "stemmer"            ],            "char_filter": [              "html_strip"            ],            "type": "custom",            "tokenizer": "standard"          }        }      }    }  }  }'
+index_settings='{  "settings": {    "index": {      "analysis": {        "filter": {          "stemmer": {            "type": "stemmer",            "language": "english"          },          "autocompleteFilter": {            "max_shingle_size": "5",            "min_shingle_size": "2",            "type": "shingle"          },          "stopwords": {            "type": "stop",            "stopwords": [              "_english_"            ]          }        },        "analyzer": {          "didYouMean": {            "filter": [              "lowercase"            ],            "type": "custom",            "tokenizer": "standard"          },          "autocomplete": {            "filter": [              "lowercase",              "autocompleteFilter"            ],            "type": "custom",            "tokenizer": "standard"          },          "default": {            "filter": [              "lowercase",              "stopwords",              "stemmer"            ],         "type": "custom",            "tokenizer": "standard"          }        }      }    }  }  }'
 
 def es_suggestion(search_string):
     return '{"suggest":{"didYouMean":{"text":"%s","phrase":{"field":"did_you_mean"}}},"query":{"match":{"question":"%s"}}}' % (search_string, search_string)
@@ -14,4 +15,6 @@ def es_fuzzy_match(search_string):
 
 def es_builder(hosts=None):
     if (hosts == None): hosts = '127.0.0.1'
+    logger = logging.getLogger('elasticsearch')
+    logger.setLevel(logging.WARNING)
     return Elasticsearch(hosts=hosts, timeout=600, request_timeout=600)
