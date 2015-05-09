5770702a88dba49174fba088e17830c65b483ff2
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-05-09 10:54:43 +0200

    Fix permutation test; use nosetests again

--- ci_build.py
+++
@@ -1,5 +0,0 @@
-import nose
-from opencricket.chart.syntax_cache import SyntaxCache
-
-SyntaxCache().build_cache()
-nose.run()
--- tests/test_permutation.py
+++ tests/test_permutation.py
@@ -5,10 +5,10 @@ from opencricket.chart import syntax_expansions
 
 class TestPermutation(unittest.TestCase):
     def test_permutation(self):
-        self.assertEqual(len(syntax_expansions.permutate_filters(['match_type', 'series', 'word_in year', 'ground'])), 65)
+        self.assertEqual(len(syntax_expansions.permutate_filters(['match_type', 'series', 'word_in year', 'ground'])), 113)
 
     def test_should_only_permutate_upto_3_filter_items(self):
-        self.assertEqual(len(syntax_expansions.permutate_filters(['match_type', 'series', 'word_in year', 'ground', 'clause_between_teams', 'clause_result_by_team'])), 227)
+        self.assertEqual(len(syntax_expansions.permutate_filters(['match_type', 'series', 'word_in year', 'ground', 'clause_between_teams', 'clause_result_by_team'])), 827)
 
 
 if __name__ == '__main__':
