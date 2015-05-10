d22f4ce234975344f41f29485b4901cf5d4701ac
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-05-10 13:04:56 +0200

    Played by two teams instead of between teams

--- opencricket/config/word_config.py
+++ opencricket/config/word_config.py
@@ -31,7 +31,8 @@ team_player_list = join_for_config(split_and_form_list(team_player_list))
 
 match_clauses = {
     'clause_result_by_team': ['word_won_lost word_by team'],
-    'clause_played_by_team': 'word_played word_by team',
+    'clause_played_by_team': ['word_played word_by team',
+                              'word_played word_by team_A word_and team_B'],
     'clause_result_against_team': 'word_won_lost word_by team_A word_against team_B',
     'clause_batting_order': 'word_batting word_batting_order',
     'clause_innings_score': 'word_scoring innings_score',
--- tests/test_matches.py
+++ tests/test_matches.py
@@ -15,6 +15,9 @@ class TestMatchesAndClauses(unittest.TestCase):
         self.input_matches_played_by_a_team = 'matches played by india batting second in 2011'
         self.expected_matches_played_by_a_team = '{"matches": {"word_in": "in", "year": "2011", "clause_batting_order": {"word_batting": "batting", "word_batting_order": "second"}, "clause_played_by_team": {"word_played": "played", "word_by": "by", "team": {"team1": "india"}}, "word_matches": "matches"}}'
 
+        self.input_matches_played_by_two_teams = 'matches played by india and sri lanka'
+        self.expected_matches_played_by_two_teams = '{"matches": {"clause_played_by_team": {"word_played": "played", "team_B": {"team": {"team2": "lanka", "team1": "sri"}}, "word_by": "by", "word_and": "and", "team_A": {"team": {"team1": "india"}}}, "word_matches": "matches"}}'
+
         self.input_matches_won_by_a_team_against_a_team = 'matches won by india against pakistan in world cup'
         self.expected_matches_won_by_a_team_against_a_team = '{"matches": {"word_in": "in", "series": {"series1": "world", "series2": "cup"}, "clause_result_against_team": {"word_won_lost": "won", "word_by": "by",  "team_A": {"team": {"team1": "india"}},  "team_B": {"team": {"team1": "pakistan"}}, "word_against": "against"}, "word_matches": "matches"}}'
 
@@ -37,6 +40,11 @@ class TestMatchesAndClauses(unittest.TestCase):
     def test_matches_played_by_a_team(self):
         parser = SentenceParser(self.input_matches_played_by_a_team)
         self.assertEqual(json.loads(self.expected_matches_played_by_a_team), json.loads(parser.parse_sentence()))
+
+    def test_matches_played_by_two_teams(self):
+        parser = SentenceParser(self.input_matches_played_by_two_teams)
+        print(json.loads(parser.parse_sentence()))
+        self.assertEqual(json.loads(self.expected_matches_played_by_two_teams), json.loads(parser.parse_sentence()))
 
     def test_matches_won_by_a_team_against_a_team(self):
         parser = SentenceParser(self.input_matches_won_by_a_team_against_a_team)
