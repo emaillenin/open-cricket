9312e680b4a7db56e9196282145dc51535b3d6b2
Lenin Raj Rajasekaran <emaillenin@gmail.com>
2015-06-26 18:34:30 +0530

    Possible filters modified

--- opencricket/suggestion/productions.py
+++ opencricket/suggestion/productions.py
@@ -31,7 +31,11 @@ class Productions:
         result = []
         parser = SentenceParser('')
         possible_filters = word_config.expandable_filters + list(word_config.match_clauses.keys()) + \
-                           ['word_this_last', 'word_against', 'word_captain', 'words_wicket_keeper', 'single', 'word_wkt_order']
+                           ['word_this_last', 'word_against', 'word_captain',
+                            'words_wicket_keeper', 'words_single_innings',
+                            'words_single_match', 'words_single_series',
+                            'words_single_year', 'words_single_ground', 'word_batting',
+                            'word_wkt_order']
         expansion_files = list(
             os.path.splitext(basename(f))[0] for f in glob.iglob(os.path.join(expansions_dir, '*.txt')))
         for stats_parser in parser.cfg_parsers:
